80ddfbb1a4367ee818f88b5dc4907101959a68cd
lsky_walt <lsky_walt@163.com>
2020-06-08 10:00:27 +0800

    feat: complete webpack config

--- webpack/webpack.pro.config.js
+++ webpack/webpack.pro.config.js
@@ -1,8 +1,30 @@
 const path = require('path')
-const webpack = require('webpack')
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-
+  entry: {
+    index: './src/index.tsx'
+  },
+  output: {
+    filename: 'index.monaco.editor.js',
+    path: path.resolve(__dirname, '../dist/')
+  },
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: ['babel-loader', 'ts-loader'],
+      exclude: /node_modules/
+    }]
+  },
+  externals: {
+    shineout: 'window.Shineout',
+    react: 'window.React',
+    'react-dom': 'window.ReactDOM',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }
