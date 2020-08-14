ceba1894e9cf93680af72db2e1d2eb6aba6bb578
lsky_walt <lsky_walt@163.com>
2020-08-14 18:04:36 +0800

    feat: auto-size

--- index.d.ts
+++ index.d.ts
@@ -20,3 +20,57 @@ export interface MonacoEditorProps {
 declare class MonacoEditor extends React.Component<MonacoEditorProps> {
   render(): JSX.Element
 }
+
+export type Size = {
+  height: number;
+  width: number;
+};
+
+export type AutoSizerProps = {
+  children: (props: Size) => React.ReactNode;
+  className?: string;
+  defaultHeight?: number;
+  defaultWidth?: number;
+  disableHeight?: boolean;
+  disableWidth?: boolean;
+  nonce?: string;
+  onResize?: (info: Size) => any;
+  style?: React.CSSProperties;
+  [key: string]: any;
+};
+
+export class AutoSizer extends PureComponent<AutoSizerProps, Size> {
+  static defaultProps: {
+      onResize: () => void;
+      disableHeight: false;
+      disableWidth: false;
+      style: {};
+  }
+
+  constructor(props: AutoSizerProps)
+
+  componentDidMount(): void
+
+  componentWillUnmount(): void
+
+  render(): JSX.Element
+}
+
+
+export interface DiffProps {
+  width?: number | string,
+  height?: number | string,
+  original: string,
+  modified: string,
+  originalLanguage?: string,
+  modifiedLanguage?: string,
+  language: string,
+  theme?: string,
+  options?: object,
+  editorWillMount?: (monaco: any) => void,
+  editorDidMount?: (original: (value: string) => void, modified: (value: string) => void, editor: any) => void,
+}
+
+export class DiffEditor extends React.Component<MonacoEditorProps> {
+  render(): JSX.Element
+}
